d59f3c21184ce50e08df01e34b201ad2ce3dc748
David Brophy <dave@davidbrophy.com>
2012-08-07 11:33:17 +0100

    Delete all spatial sql updates

--- Bobs/Sql/Update Scripts/0203 - create procs for creating SpatialIndex columns.sql
+++
@@ -1,8 +0,0 @@
-﻿IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[AddSpatialColumnsToTable]') AND type in (N'P', N'PC'))
-DROP PROCEDURE [dbo].[AddSpatialColumnsToTable]
-go
- 
-IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[AddSpatialIndexToTable]') AND type in (N'P', N'PC'))
-DROP PROCEDURE [dbo].[AddSpatialIndexToTable]
-go
- 
